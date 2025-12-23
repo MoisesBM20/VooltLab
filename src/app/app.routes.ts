e5f7208b97afa18335569e6fae226bc06d0d0bca
MoisesBM <moisesbuitrago201@gmail.com>
2025-12-22 21:02:47 -0500

    LandingPage V1.3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home').then(m => m.Home),
+    title: 'VooltLab | Soluciones Tecnologicas' // Título para la pestaña del navegador (Angular 14+)
+  },
+  // En el futuro agregarás:
+  // { path: 'servicios', loadComponent: ... },
+  {
+    path: '**', // Ruta comodín para 404 (opcional por ahora)
+    redirectTo: ''
+  }
+];
---
+++ src/app/layout/navbar/navbar.ts
@@ -0,0 +1,18 @@
+import { Component, HostListener } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive], // Importante agregar esto
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {
+isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 20;
+  }
+}
